30c1fcba88965f662665191ea47c9b40c4f35d18
alish1999 <alishbhosale@gmail.com>
2024-12-02 20:35:35 +0530

    Type checking payment info

--- src/components/Step3.jsx
+++ src/components/Step3.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, Select, DatePicker } from "antd";
+import dayjs from "dayjs";
+
+const { Option } = Select;
 
 const Step3 = ({ data, onUpdate, form }) => {
   const onValuesChange = () => {
-    onUpdate(form.getFieldsValue());
+    onUpdate({...form.getFieldsValue(),expiryDate:form?.getFieldValue('expiryDate')?.format('MM/YYYY')});
   };
 
   return (
@@ -13,32 +16,58 @@ const Step3 = ({ data, onUpdate, form }) => {
       initialValues={data}
       onValuesChange={onValuesChange}
     >
+      {/* Credit Card Number */}
       <Form.Item
         label="Credit Card Number"
         name="cardNumber"
         rules={[
           { required: true, message: "Card number is required" },
-        //   { len: 16, message: "Card number must be 16 digits" },
+          {
+            pattern: /^\d{16}$/,
+            message: "Card number must be 16 digits",
+          },
         ]}
       >
-        <Input />
+        <Input maxLength={16} />
       </Form.Item>
+
+      {/* Expiry Date */}
       <Form.Item
         label="Expiry Date"
         name="expiryDate"
-        rules={[{ required: true, message: "Expiry Date is required" }]}
+        rules={[
+          { required: true, message: "Expiry Date is required" },
+          {
+            validator: (_, value) =>
+              value && value.isAfter(dayjs())
+                ? Promise.resolve()
+                : Promise.reject("Expiry date must be a future date"),
+          },
+        ]}
       >
-        <Input />
+        <DatePicker
+          format="MM/YYYY"
+          picker="month"
+          disabledDate={(current) =>
+            current && current.isBefore(dayjs(), "month")
+          }
+        //   onChange={(value) => form.setFieldsValue({ expiryDate: value })}
+        />
       </Form.Item>
+
+      {/* CVV */}
       <Form.Item
         label="CVV"
         name="cvv"
         rules={[
           { required: true, message: "CVV is required" },
-          { len: 3, message: "CVV must be 3 digits" },
+          {
+            pattern: /^\d{3}$/,
+            message: "CVV must be 3 digits",
+          },
         ]}
       >
-        <Input />
+        <Input maxLength={3} />
       </Form.Item>
     </Form>
   );
